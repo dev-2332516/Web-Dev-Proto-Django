dfdd8bacb89ae76dde62dfa1c4be03adb8965ace
TheyCreeper <theycreeper@proton.me>
2025-09-21 18:41:20 -0400

    feat: final touches

--- newEnv.ps1
+++ newEnv.ps1
@@ -1,5 +1,5 @@
 # Setup d'environement sur un nouveau PC
-py -m venv .venv
+python -m venv .venv
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
 .\.venv\Scripts\activate.ps1
 python -m pip install --upgrade pip
