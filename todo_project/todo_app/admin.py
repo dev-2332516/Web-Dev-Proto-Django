541ff76a0a2399b7a44ee4d27fc6a3e147ad339b
dev-2332516 <2332516@etu.cchic.ca>
2025-09-26 10:27:40 -0400

    feat: ajouter style admin

--- todo_project/todo_app/admin.py
+++ todo_project/todo_app/admin.py
@@ -1,14 +1,26 @@
 from django.contrib import admin
 from .models import Todo
+from .models import Category
 
-admin.site.register(Todo)
-#admin.site.register(Product)
-
-
-# @admin.register(Product)
-# class ProductAdmin(admin.ModelAdmin):
-#     list_display = ['code', 'name',]
-#     list_filter = ['category',]
-#     #ordering = ['name',]
-#     #readonly_fields = ['code']
-#     search_fields = ['code', 'name']
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'category', 'description', 'isDone')
+    list_filter = ('category', 'isDone')
+    search_fields = ('name', 'category')
+    list_editable = ('name', 'category', 'description', 'isDone')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'category', 'description', 'isDone')
+        }),
+    )
+    
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'color')
+    search_fields = ('name', 'color')
+    list_editable = ('name', 'color')
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'color')
+        }),
+    )
